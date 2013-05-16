1bd69559b7b4b40c54b828ced3ae54f00d7921b9
WooCaSh <zlukasz.poczta@gmail.com>
2013-05-16 17:42:35 -0400

    create files

---
+++ js/jQuery.Anchor3D.js
@@ -0,0 +1,45 @@
+﻿(function ($) {
+    $.fn.anchor3D = function (options) {
+        var defaultVal = {
+            linkBg: "",
+            linkHoverBg: "",
+            linkColor: "",
+            linkHoverColor: "",
+            enableReminder: false,
+            effect: ""//['roll', 'moveToLeft']
+        };
+
+        var latency = 5000;
+
+        reminder = function (link) {
+            var linkObj = $(link).parent().parent();
+            linkObj.addClass('hovered');
+            setTimeout(function () {
+                linkObj.removeClass('hovered');
+            }, latency / 2);
+        };
+
+        var obj = $.extend(defaultVal, options);
+
+        return this.each(function () {
+            var effectClass = defaultVal.effect !== "" ? defaultVal.effect : "roll";
+            $(this).addClass(effectClass);
+            $(this).addClass('niceAnchor');
+            
+            var anchorTxt = $(this).text();
+            var innerSpan = "<span class='innerSpan'>" + anchorTxt + "</span>";
+            var outerSpan = "<span class='outerSpan'>" + anchorTxt + innerSpan + "</span>";
+            
+            var link = $(this).html(outerSpan).find("span.outerSpan")
+                .css('backgroundColor', defaultVal.linkBg)
+                .css('color', defaultVal.linkColor)
+                .find("span.innerSpan")
+                .css('backgroundColor', defaultVal.linkHoverBg)
+                .css('color', defaultVal.linkHoverColor);
+
+            if (defaultVal.enableReminder) {
+                setInterval(function () { reminder(link) }, latency);
+            }
+        });
+    };
+})(jQuery);
---
+++ js/test.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $("a.test1, a.test3").anchor3D({
+        linkHoverBg: "blue",
+        linkHoverColor: 'yellow',
+        effect: 'moveToLeft'
+    });
+
+    $("a.test2").anchor3D({
+        linkHoverBg: "#ccc",
+        linkHoverColor: '#000',
+        enableReminder: true
+    });
+});
